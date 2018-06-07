58d87f9e23d84ef408dc601f602f05f36c2fcbe6
sanjeet.yadav <sanjeet.yadav@gspann.com>
2018-06-07 15:27:32 +0530

    iTrack-42-resource ui-changes

--- src/main/webapp/app/resource/add-resource/add-resource.component.ts
+++ src/main/webapp/app/resource/add-resource/add-resource.component.ts
@@ -9,29 +9,21 @@ import { FormControl, FormsModule, FormBuilder, FormGroupDirective, FormGroup, N
 export class AddResourceComponent implements OnInit {
     resourceForm: FormGroup;
     FTEFields: Array<any>;
+    contractorFields: Array<any>;
     constructor(private fb: FormBuilder) {}
 
     ngOnInit() {
         this.resourceForm = this.fb.group({
-            resourceName: new FormControl('', []),
-            companyName: new FormControl('', []),
-            department: new FormControl('', []),
+            resourceName: new FormControl('', [Validators.required]),
+            companyName: new FormControl('', [Validators.required]),
+            department: new FormControl('', [Validators.required]),
+            designation: new FormControl('', [Validators.required]),
             joiningDate: new FormControl('', [Validators.required]),
             actualJoiningDate: new FormControl('', [Validators.required]),
             baseLocation: new FormControl('', []),
             deputedLocation: new FormControl('', []),
-            employeeType: new FormControl('', [])
-            // customerManager: new FormControl('', [Validators.required]),
-            // projectName: new FormControl('', [Validators.required]),
-            // projectTypeCode: new FormControl('', [Validators.required]),
-            // cityId: new FormControl('', [Validators.required]),
-            // startDate: new FormControl('', [Validators.required]),
-            // endDate: new FormControl('', [Validators.required]),
-            // offshoreManagerCode: new FormControl('', [Validators.required]),
-            // onshoreManagerCode: new FormControl('', [Validators.required]),
-            // practices: new FormControl('', [Validators.required]),
-            // projectStatusCode: new FormControl('', [Validators.required]),
-            // technology: new FormControl('', [Validators.required])
+            employeeType: new FormControl('', []),
+            empStatus: new FormControl('', [Validators.required])
         });
 
         this.FTEFields = [
@@ -46,18 +38,51 @@ export class AddResourceComponent implements OnInit {
             {
                 name: 'commision',
                 validators: [Validators.required]
+            },
+            {
+                name: 'bonus',
+                validators: [Validators.required]
+            }
+        ];
+
+        this.contractorFields = [
+            {
+                name: 'currency',
+                validators: [Validators.required]
+            },
+            {
+                name: 'ratePerHour',
+                validators: [Validators.required]
+            },
+            {
+                name: 'startDate',
+                validators: [Validators.required]
+            },
+            {
+                name: 'endDate',
+                validators: [Validators.required]
+            },
+            {
+                name: 'remarks',
+                validators: []
             }
         ];
 
         this.resourceForm.get('employeeType').valueChanges.subscribe(value => {
+            let self = this;
             if (value === '1') {
-                let self = this;
+                this.contractorFields.map(index => {
+                    this.resourceForm.removeControl(index.name);
+                });
                 this.FTEFields.map(index => {
                     self.resourceForm.addControl(index.name, new FormControl('', index.validators));
                 });
-            } else if (value === '2') {
+            } else if (value === '2' || value === '3') {
                 this.FTEFields.map(index => {
                     this.resourceForm.removeControl(index.name);
+                });
+                this.contractorFields.map(index => {
+                    self.resourceForm.addControl(index.name, new FormControl('', index.validators));
                 });
             }
         });
