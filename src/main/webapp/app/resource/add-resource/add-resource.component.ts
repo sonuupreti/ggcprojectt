c500ee3076789f9eeb3de458da26f1c0cfdcb34a
sanjeet.yadav <sanjeet.yadav@gspann.com>
2018-06-07 11:04:36 +0530

    iTrack-42-resource ui-changes

--- src/main/webapp/app/resource/add-resource/add-resource.component.ts
+++ src/main/webapp/app/resource/add-resource/add-resource.component.ts
@@ -8,17 +8,19 @@ import { FormControl, FormsModule, FormBuilder, FormGroupDirective, FormGroup, N
 })
 export class AddResourceComponent implements OnInit {
     resourceForm: FormGroup;
+    FTEFields: Array<any>;
     constructor(private fb: FormBuilder) {}
 
     ngOnInit() {
         this.resourceForm = this.fb.group({
-            resourceName: new FormControl('', [Validators.required]),
-            companyName: new FormControl('', [Validators.required]),
-            department: new FormControl('', [Validators.required]),
+            resourceName: new FormControl('', []),
+            companyName: new FormControl('', []),
+            department: new FormControl('', []),
             joiningDate: new FormControl('', [Validators.required]),
             actualJoiningDate: new FormControl('', [Validators.required]),
             baseLocation: new FormControl('', []),
-            deputedLocation: new FormControl('', [])
+            deputedLocation: new FormControl('', []),
+            employeeType: new FormControl('', [])
             // customerManager: new FormControl('', [Validators.required]),
             // projectName: new FormControl('', [Validators.required]),
             // projectTypeCode: new FormControl('', [Validators.required]),
@@ -31,5 +33,35 @@ export class AddResourceComponent implements OnInit {
             // projectStatusCode: new FormControl('', [Validators.required]),
             // technology: new FormControl('', [Validators.required])
         });
+
+        this.FTEFields = [
+            {
+                name: 'anualPackage',
+                validators: [Validators.required]
+            },
+            {
+                name: 'currency',
+                validators: [Validators.required]
+            },
+            {
+                name: 'commision',
+                validators: [Validators.required]
+            }
+        ];
+
+        this.resourceForm.get('employeeType').valueChanges.subscribe(value => {
+            if (value === '1') {
+                let self = this;
+                this.FTEFields.map(index => {
+                    self.resourceForm.addControl(index.name, new FormControl('', index.validators));
+                });
+            } else if (value === '2') {
+                this.FTEFields.map(index => {
+                    this.resourceForm.removeControl(index.name);
+                });
+            }
+        });
+
+        this.resourceForm.get('employeeType').setValue('1');
     }
 }
