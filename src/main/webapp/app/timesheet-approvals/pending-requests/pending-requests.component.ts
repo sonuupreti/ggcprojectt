e0f622ab3d673e8a70ad393acb17777ea262d338
Shamshuddin <akbar.shaik@gspann.com>
2018-06-12 10:07:50 +0530

    ITRACK-48: Timesheet Requests-> All requests updates

--- src/main/webapp/app/timesheet-approvals/pending-requests/pending-requests.component.ts
+++ src/main/webapp/app/timesheet-approvals/pending-requests/pending-requests.component.ts
@@ -42,7 +42,7 @@ export class PendingRequestsComponent implements OnInit {
                         remarks: null,
                         timeEntries: {
                             hours: 8,
-                            comments: 'iTrack standup'
+                            comments: ''
                         }
                     },
                     {
@@ -68,7 +68,7 @@ export class PendingRequestsComponent implements OnInit {
                         remarks: null,
                         timeEntries: {
                             hours: 8,
-                            comments: 'iTrack standup'
+                            comments: ''
                         }
                     },
                     {
@@ -81,7 +81,7 @@ export class PendingRequestsComponent implements OnInit {
                         remarks: 'Diwali',
                         timeEntries: {
                             hours: 8,
-                            comments: 'iTrack standup'
+                            comments: ''
                         }
                     },
                     {
@@ -94,7 +94,7 @@ export class PendingRequestsComponent implements OnInit {
                         remarks: null,
                         timeEntries: {
                             hours: 8,
-                            comments: 'iTrack standup'
+                            comments: ''
                         }
                     },
                     {
@@ -120,7 +120,7 @@ export class PendingRequestsComponent implements OnInit {
                         remarks: null,
                         timeEntries: {
                             hours: 8,
-                            comments: 'iTrack standup'
+                            comments: ''
                         }
                     }
                 ]
@@ -147,7 +147,7 @@ export class PendingRequestsComponent implements OnInit {
                             date: '2018-05-21',
                             timeEntries: {
                                 hours: 8,
-                                comments: 'iTrack standup'
+                                comments: ''
                             }
                         },
                         {
@@ -168,14 +168,14 @@ export class PendingRequestsComponent implements OnInit {
                             date: '2018-05-24',
                             timeEntries: {
                                 hours: 4,
-                                comments: 'iTrack standup'
+                                comments: ''
                             }
                         },
                         {
                             date: '2018-05-25',
                             timeEntries: {
                                 hours: 4,
-                                comments: 'iTrack standup'
+                                comments: ''
                             }
                         },
                         {
@@ -222,7 +222,328 @@ export class PendingRequestsComponent implements OnInit {
                             date: '2018-05-22',
                             timeEntries: {
                                 hours: 0,
+                                comments: ''
+                            }
+                        },
+                        {
+                            date: '2018-05-23',
+                            timeEntries: {
+                                hours: 8,
+                                comments: ''
+                            }
+                        },
+                        {
+                            date: '2018-05-24',
+                            timeEntries: {
+                                hours: 4,
                                 comments: 'iTrack standup'
+                            }
+                        },
+                        {
+                            date: '2018-05-25',
+                            timeEntries: {
+                                hours: 4,
+                                comments: 'iTrack standup'
+                            }
+                        },
+                        {
+                            date: '2018-05-26',
+                            timeEntries: {
+                                hours: 4,
+                                comments: 'iTrack standup'
+                            }
+                        },
+                        {
+                            date: '2018-05-27',
+                            timeEntries: {
+                                hours: 4,
+                                comments: ''
+                            }
+                        }
+                    ]
+                },
+                {
+                    project: {
+                        key: 'INV0000001',
+                        value: 'UNPAID LEAVE'
+                    },
+                    projectType: {
+                        key: 'INV',
+                        value: 'Investment'
+                    },
+                    actions: {
+                        isApproved: false,
+                        isCancelled: true
+                    },
+                    proportion: 50,
+                    customerTimeTracking: false,
+                    totalSubmittedHrs: 20,
+                    dailyEntries: [
+                        {
+                            date: '2018-05-21',
+                            timeEntries: {
+                                hours: 0,
+                                comments: ''
+                            }
+                        },
+                        {
+                            date: '2018-05-22',
+                            timeEntries: {
+                                hours: 0,
+                                comments: 'iTrack standup'
+                            }
+                        },
+                        {
+                            date: '2018-05-23',
+                            timeEntries: {
+                                hours: 8,
+                                comments: ''
+                            }
+                        },
+                        {
+                            date: '2018-05-24',
+                            timeEntries: {
+                                hours: 4,
+                                comments: 'iTrack standup'
+                            }
+                        },
+                        {
+                            date: '2018-05-25',
+                            timeEntries: {
+                                hours: 4,
+                                comments: ''
+                            }
+                        },
+                        {
+                            date: '2018-05-26',
+                            timeEntries: {
+                                hours: 4,
+                                comments: ''
+                            }
+                        },
+                        {
+                            date: '2018-05-27',
+                            timeEntries: {
+                                hours: 4,
+                                comments: 'iTrack standup'
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            status: 'Not Submitted',
+            approverAction: 'N/A',
+            approverComments: 'N/A',
+            resource: {
+                key: '20001',
+                value: 'Manoj Nautiyal'
+            },
+            weekDetails: {
+                weekStartDate: '2018-05-21',
+                weekEndDate: '2018-05-27',
+                submittedDate: '2018-05-27',
+                weekLength: 7,
+                week: 36,
+                weekStartDay: 'MONDAY',
+                weekEndDay: 'SUNDAY',
+                dailyStandardHours: 8,
+                weeklyStandardHours: 40,
+                flexibleWeekends: false,
+                totalWeekSubmitedHours: 60,
+                dailyDetails: [
+                    {
+                        date: '2018-05-21',
+                        day: 'MONDAY',
+                        type: {
+                            code: 'WD',
+                            value: 'Regular Working Day'
+                        },
+                        remarks: null,
+                        timeEntries: {
+                            hours: 8,
+                            comments: ''
+                        }
+                    },
+                    {
+                        date: '2018-05-22',
+                        day: 'TUESDAY',
+                        type: {
+                            code: 'WD',
+                            value: 'Regular Working Day'
+                        },
+                        remarks: null,
+                        timeEntries: {
+                            hours: 8,
+                            comments: 'iTrack standup'
+                        }
+                    },
+                    {
+                        date: '2018-05-23',
+                        day: 'WEDNESDAY',
+                        type: {
+                            code: 'WD',
+                            value: 'Regular Working Day'
+                        },
+                        remarks: null,
+                        timeEntries: {
+                            hours: 8,
+                            comments: ''
+                        }
+                    },
+                    {
+                        date: '2018-05-24',
+                        day: 'THURSDAY',
+                        type: {
+                            code: 'HD',
+                            value: 'Holiday'
+                        },
+                        remarks: 'Diwali',
+                        timeEntries: {
+                            hours: 8,
+                            comments: 'iTrack standup'
+                        }
+                    },
+                    {
+                        date: '2018-05-25',
+                        day: 'FRIDAY',
+                        type: {
+                            code: 'WD',
+                            value: 'Regular Working Day'
+                        },
+                        remarks: null,
+                        timeEntries: {
+                            hours: 8,
+                            comments: ''
+                        }
+                    },
+                    {
+                        date: '2018-05-26',
+                        day: 'SATURDAY',
+                        type: {
+                            code: 'WE',
+                            value: 'Weekend'
+                        },
+                        remarks: null,
+                        timeEntries: {
+                            hours: 8,
+                            comments: 'iTrack standup'
+                        }
+                    },
+                    {
+                        date: '2018-05-27',
+                        day: 'SUNDAY',
+                        type: {
+                            code: 'WE',
+                            value: 'Weekend'
+                        },
+                        remarks: null,
+                        timeEntries: {
+                            hours: 8,
+                            comments: ''
+                        }
+                    }
+                ]
+            },
+            projects: [
+                {
+                    project: {
+                        key: 'INV0000004',
+                        value: 'iTrack'
+                    },
+                    projectType: {
+                        key: 'INV',
+                        value: 'Investment'
+                    },
+                    actions: {
+                        isApproved: true,
+                        isCancelled: false
+                    },
+                    proportion: 50,
+                    customerTimeTracking: false,
+                    totalSubmittedHrs: 20,
+                    dailyEntries: [
+                        {
+                            date: '2018-05-21',
+                            timeEntries: {
+                                hours: 8,
+                                comments: ''
+                            }
+                        },
+                        {
+                            date: '2018-05-22',
+                            timeEntries: {
+                                hours: 8,
+                                comments: ''
+                            }
+                        },
+                        {
+                            date: '2018-05-23',
+                            timeEntries: {
+                                hours: 0,
+                                comments: 'iTrack standup'
+                            }
+                        },
+                        {
+                            date: '2018-05-24',
+                            timeEntries: {
+                                hours: 4,
+                                comments: 'iTrack standup'
+                            }
+                        },
+                        {
+                            date: '2018-05-25',
+                            timeEntries: {
+                                hours: 4,
+                                comments: ''
+                            }
+                        },
+                        {
+                            date: '2018-05-26',
+                            timeEntries: {
+                                hours: 4,
+                                comments: ''
+                            }
+                        },
+                        {
+                            date: '2018-05-27',
+                            timeEntries: {
+                                hours: 4,
+                                comments: 'iTrack standup'
+                            }
+                        }
+                    ]
+                },
+                {
+                    project: {
+                        key: 'INV0000005',
+                        value: 'sample'
+                    },
+                    projectType: {
+                        key: 'INV',
+                        value: 'Investment'
+                    },
+                    actions: {
+                        isApproved: false,
+                        isCancelled: true
+                    },
+                    proportion: 50,
+                    customerTimeTracking: false,
+                    totalSubmittedHrs: 20,
+                    dailyEntries: [
+                        {
+                            date: '2018-05-21',
+                            timeEntries: {
+                                hours: 0,
+                                comments: 'iTrack standup'
+                            }
+                        },
+                        {
+                            date: '2018-05-22',
+                            timeEntries: {
+                                hours: 0,
+                                comments: ''
                             }
                         },
                         {
@@ -257,7 +578,7 @@ export class PendingRequestsComponent implements OnInit {
                             date: '2018-05-27',
                             timeEntries: {
                                 hours: 4,
-                                comments: 'iTrack standup'
+                                comments: ''
                             }
                         }
                     ]
@@ -283,7 +604,7 @@ export class PendingRequestsComponent implements OnInit {
                             date: '2018-05-21',
                             timeEntries: {
                                 hours: 0,
-                                comments: 'iTrack standup'
+                                comments: ''
                             }
                         },
                         {
@@ -304,7 +625,7 @@ export class PendingRequestsComponent implements OnInit {
                             date: '2018-05-24',
                             timeEntries: {
                                 hours: 4,
-                                comments: 'iTrack standup'
+                                comments: ''
                             }
                         },
                         {
@@ -335,6 +656,7 @@ export class PendingRequestsComponent implements OnInit {
     ];
     constructor(private timesheetApprovalService: TimesheetApprovalsService, private config: NgbPopoverConfig) {
         config.placement = 'top';
+        // config.triggers = 'click';
         config.triggers = 'hover';
     }
 
